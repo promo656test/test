36dabe8415470ce28e1103c411897cd35cd467be
Egor <egormarkov1408@gmail.com>
2025-03-14 15:17:24 -0300

    test

--- src/members-permissions.ts
+++ src/members-permissions.ts
@@ -1,37 +1,23 @@
 import type { PermissionsConfig } from "./types";
 
 const membersPermissions: PermissionsConfig = {
-  users: ['promo656','espostnov', 'user1', 'user2', 'user3'],
+  users: ['espostnov', 'user1', 'user2', 'user3'],
 
   organizations: [
     {
-      name: 'web3-test-organisation-3',
-      repositories: ['test3-repo-1', 'test3-repo-2', ],
+      name: 'promo656test',
+      repositories: ['test'],
       teams: [
         {
           name: 'team1',
           members: ['espostnov'],
           repositories: [
-            { name: 'test3-repo-1', permissions: ['admin'] },
-            { name: 'test3-repo-2', permissions: ['write'] }
+            { name: 'test3-repo-1', permissions: ['read'] },
+
           ]
         }
       ]
     },
-    {
-      name: 'web3-test-organisation-2',
-      repositories: ['test2-repo-1', 'test2-repo-2', ],
-      teams: [
-        {
-          name: 'team1',
-          members: ['espostnov'],
-          repositories: [
-            { name: 'test2-repo-1', permissions: [ 'write' ] },
-            { name: 'test2-repo-2', permissions: [ 'read' ] }
-          ]
-        }
-      ]
-    }
   ]
 };
 
